574c634fe894aed2697fc11c16a2beeb671e33fe
shalev <shalev56@gmail.com>
2025-04-03 00:04:19 +0300

    process env

--- config/secret.js
+++ config/secret.js
@@ -4,10 +4,7 @@ exports.confsecretList = {
     PORT:process.env.PORT,
     TOKEN_SECRET:process.env.JWT_SECRET_KEY,
     USERNAME:process.env.USERNAME,
-    PASSWORD:process.env.PASSWORD,
-    USER_ID:process.env.USER_ID,
-    SERVICE_ID:process.env.SERVICE_ID,
-    TEMPLATE_ID:process.env.TEMPLATE_ID
+    PASSWORD:process.env.PASSWORD
     
 
 }
